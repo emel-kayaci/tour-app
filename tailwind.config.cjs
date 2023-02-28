0415ffc12f3ee8438486f58495f660fcbad927a3
Emel Kayaci <kayaciemel18@gmail.com>
2023-02-28 16:58:15 +0300

    configure tailwind, include images, add navbar

---
+++ tailwind.config.cjs
@@ -0,0 +1,38 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        "gray-20": "#F8F8FF",
+        "gray-50": "#DCEAFE",
+        "gray-100": "#CED7E4",
+        "gray-500": "#454545",
+        "primary-100": "#FFE285",
+        "primary-300": "#F3C42F",
+        "primary-500": "#E4B41B",
+        "secondary-400": "#C6C8FF",
+        "secondary-500": "#6569D9",
+      },
+      backgroundImage: (theme) => ({
+        "gradient-purple": "linear-gradient(90deg, #484A66 0%, #F3F4FF 100%)",
+        "mobile-home": "url('./assets/HomePage.png')",
+      }),
+      fontFamily: {
+        dmsans: ["DM Sans", "sans-serif"],
+        montserrat: ["Montserrat", "sans-serif"],
+      },
+      content: {
+        evolvetext: "url('./assets/TravelExpressText.png')",
+        abstractwaves: "url('./assets/AbstractWaves.png')",
+        places: "url('./assets/Places.png')",
+      },
+    },
+    screens: {
+      xs: "480px",
+      sm: "768px",
+      md: "1060px",
+    },
+  },
+  plugins: [],
+};
